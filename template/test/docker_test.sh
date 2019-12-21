0614dd6a2172b1ac7d3f39f57929189b6d03abb6
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-12-21 02:48:20 +0100

    :green_heart: Remove sudo usage in tests
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- template/test/docker_test.sh
+++ template/test/docker_test.sh
@@ -6,7 +6,7 @@ echo "Waiting to ensure everything is fully ready for the tests..."
 sleep 60
 
 echo "Checking main containers are reachable..."
-if ! sudo ping -c 10 -q postgres ; then
+if ! ping -c 10 -q postgres ; then
     echo 'Apache Guacamole Server Database container is not responding!'
     # TODO Display logs to help bug fixing
     #echo 'Check the following logs for details:'
@@ -14,7 +14,7 @@ if ! sudo ping -c 10 -q postgres ; then
     exit 2
 fi
 
-if ! sudo ping -c 10 -q guacd ; then
+if ! ping -c 10 -q guacd ; then
     echo 'Apache Guacamole Server container is not responding!'
     # TODO Display logs to help bug fixing
     #echo 'Check the following logs for details:'
@@ -22,7 +22,7 @@ if ! sudo ping -c 10 -q guacd ; then
     exit 4
 fi
 
-if ! sudo ping -c 10 -q guacamole ; then
+if ! ping -c 10 -q guacamole ; then
     echo 'Apache Guacamole Frontend container is not responding!'
     # TODO Display logs to help bug fixing
     #echo 'Check the following logs for details:'
